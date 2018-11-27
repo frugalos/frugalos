ef7e8cb53099e5a9cb8dbb986e15e053c6865072
Shinya Yamaoka <syamaoka@mail.libmacro.com>
2018-11-27 16:31:59 +0900

    Add default-put-content-timeout and minimum-put-content-timeout options.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 extern crate frugalos;
 extern crate frugalos_config;
+extern crate frugalos_segment;
 extern crate hostname;
 extern crate libfrugalos;
 #[macro_use]
@@ -11,10 +12,14 @@ extern crate trackable;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
 use libfrugalos::entity::server::Server;
+use libfrugalos::time::Seconds;
 use sloggers::Build;
 use std::env;
 use std::net::{SocketAddr, ToSocketAddrs};
 use trackable::error::Failure;
+
+use frugalos::{Error, Result};
+use frugalos_segment::config::MdsClientConfig;
 
 #[cfg_attr(feature = "cargo-clippy", allow(cyclomatic_complexity))]
 fn main() {
@@ -51,7 +56,9 @@ fn main() {
                         .long("http-server-bind-addr")
                         .takes_value(true)
                         .default_value("0.0.0.0:3000"),
-                ).arg(data_dir_arg()),
+                ).arg(data_dir_arg())
+                .arg(default_put_content_timeout_arg())
+                .arg(minimum_put_content_timeout_arg()),
         ).subcommand(
             SubCommand::with_name("stop").arg(
                 Arg::with_name("RPC_ADDR")
@@ -175,6 +182,8 @@ fn main() {
             matches.value_of("SAMPLING_RATE").unwrap().parse()
         ));
         daemon.sampling_rate = sampling_rate;
+        daemon.mds_client_config =
+            track_try_unwrap!(track_any_err!(get_mds_client_config(&matches)));
 
         if let Some(threads) = matches.value_of("EXECUTOR_THREADS") {
             let threads: usize = track_try_unwrap!(track_any_err!(threads.parse()));
@@ -248,6 +257,22 @@ fn data_dir_arg<'a, 'b>() -> Arg<'a, 'b> {
         .takes_value(true)
 }
 
+fn default_put_content_timeout_arg<'a, 'b>() -> Arg<'a, 'b> {
+    Arg::with_name("DEFAULT_PUT_CONTENT_TIMEOUT")
+        .help("Sets the default timeout in seconds on putting a content.")
+        .long("default-put-content-timeout")
+        .takes_value(true)
+        .default_value("60")
+}
+
+fn minimum_put_content_timeout_arg<'a, 'b>() -> Arg<'a, 'b> {
+    Arg::with_name("MINIMUM_PUT_CONTENT_TIMEOUT")
+        .help("Sets the minimum timeout in seconds on putting a content.")
+        .long("minimum-put-content-timeout")
+        .takes_value(true)
+        .default_value("60")
+}
+
 fn get_data_dir(matches: &ArgMatches) -> String {
     if let Some(value) = matches
         .value_of("DATA_DIR")
@@ -261,4 +286,30 @@ fn get_data_dir(matches: &ArgMatches) -> String {
         );
         std::process::exit(1);
     }
+}
+
+/// Gets `MdsClientConfig` from CLI arguments.
+fn get_mds_client_config(matches: &ArgMatches) -> Result<MdsClientConfig> {
+    let mut config = MdsClientConfig::default();
+    config.default_put_content_timeout = matches
+        .value_of("DEFAULT_PUT_CONTENT_TIMEOUT")
+        .map_or_else(
+            || Ok(config.default_put_content_timeout),
+            |v| {
+                v.parse::<u64>()
+                    .map(Seconds)
+                    .map_err(|e| track!(Error::from(e)))
+            },
+        )?;
+    config.minimum_put_content_timeout = matches
+        .value_of("MINIMUM_PUT_CONTENT_TIMEOUT")
+        .map_or_else(
+            || Ok(config.minimum_put_content_timeout),
+            |v| {
+                v.parse::<u64>()
+                    .map(Seconds)
+                    .map_err(|e| track!(Error::from(e)))
+            },
+        )?;
+    Ok(config)
 }
