5c0f60d9a373fbb10544b3b775aa77f9ccce2711
yuuya uezato <yuuya_uezato@dwango.co.jp>
2018-12-23 23:24:54 +0900

    frugalosをMacネイティブで動かし、prometheusはDockerで動かす場合の基本的な構成

---
+++ visualize_scripts/frugalos_on_mac/example.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eux
+
+DATADIR='frugalos_data_dir'
+EXAMPLE='example'
+FRUGALOS="../../target/debug/frugalos"
+
+$FRUGALOS create --id $EXAMPLE --data-dir $DATADIR/
+
+tmux split-window "RAFT_IO_MAX_LUMP_DATA_SIZE=512 FRUGALOS_SNAPSHOT_THRESHOLD=10 ${FRUGALOS} -l debug start --data-dir ${DATADIR}; read"
+
+sleep 5
+
+DEVICE_JSON='{"file": {"id": "file0", "server": "'$EXAMPLE'", "filepath": "'$DATADIR'/file0.lusf"}}'
+curl -XPUT -d "$DEVICE_JSON" http://localhost:3000/v1/devices/file0
+echo ""
+
+sleep 3
+
+BUCKET_JSON=$(cat <<EOF
+{
+  "dispersed": {
+    "id": "bucket0",
+    "device": "file0",
+    "tolerable_faults": 4,
+    "data_fragment_count": 6
+  }
+}
+EOF
+)
+curl -XPUT -d "$BUCKET_JSON" http://localhost:3000/v1/buckets/bucket0
+echo ""
+
+sleep 3
+
+curl -XPUT -d 'your_object_data' http://localhost:3000/v1/buckets/bucket0/objects/your_object_id
+
+sleep 3
+
+curl http://localhost:3000/v1/buckets/bucket0/objects/your_object_id
