aab9f868c0d281a28a150e1c60e7e709e3ef60a4
Shinya Yamaoka <syamaoka@mail.libmacro.com>
2019-02-04 12:09:18 +0900

    Revert "Merge pull request #70 from frugalos/explicit_jemalloc"
    This reverts commit 695eee245ad558867405dc8f7d00babd2213adee, reversing
    changes made to fc2fcd24e1cbcf8bd4faaf6939979a16a5989e1e.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate frugalos;
 extern crate frugalos_config;
 extern crate frugalos_segment;
 extern crate hostname;
-extern crate jemallocator;
 extern crate libfrugalos;
 #[macro_use]
 extern crate slog;
@@ -23,9 +22,6 @@ use trackable::error::Failure;
 
 use frugalos::{Error, Result};
 use frugalos_segment::config::MdsClientConfig;
-
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[allow(clippy::cyclomatic_complexity)]
 fn main() {
