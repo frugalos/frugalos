5336b7106b5c7f213a66998e13440c392f2878a5
Shinya Yamaoka <syamaoka@mail.libmacro.com>
2019-05-24 18:29:27 +0900

    Add a new testsuite for consistency

--- it/scripts/http_requests.sh
+++ it/scripts/http_requests.sh
@@ -7,7 +7,8 @@ EXPECTED_STATUS=$2
 REQUEST_FILE=$3
 RESPONSE_FILE=$4
 EXPECTED_TOTAL=${5:-1000} # デフォルト 1000
+QUERY_PARAMS=${QUERY_PARAMS:-""}
 
-jq "map({method: \"${METHOD}\", url: .url})" $REQUEST_FILE | hb run -o $RESPONSE_FILE
+jq "map({method: \"${METHOD}\", url: .url} | .url |= . + \"${QUERY_PARAMS}\")" $REQUEST_FILE | hb run -o $RESPONSE_FILE
 hb summary -i $RESPONSE_FILE
 [ `hb summary -i $RESPONSE_FILE | jq ".status.\"${EXPECTED_STATUS}\""` -eq "${EXPECTED_TOTAL}" ]
