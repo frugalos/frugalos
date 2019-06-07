47f5bb9b6c31ce91fddd3d9edee3817b76022267
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-06-07 18:19:22 +0900

    Stop using sink/stream (note: object_version_limit is not necessarily correct)

--- frugalos_segment/src/full_sync.rs
+++ frugalos_segment/src/full_sync.rs
@@ -1,20 +1,17 @@
 use cannyls::deadline::Deadline;
 use cannyls::device::DeviceHandle;
 use cannyls::lump::LumpId;
-#[allow(deprecated)]
-use fibers::sync::mpsc::sync_channel;
 use fibers_tasque::{DefaultCpuTaskQueue, TaskQueueExt};
 use frugalos_mds::machine::Machine;
 use frugalos_raft::NodeId;
 use futures::future::{loop_fn, ok};
 use futures::future::{Future, Loop};
-use futures::stream::Stream;
-use futures::{Poll, Sink};
+use futures::Poll;
 use libfrugalos::entity::object::ObjectVersion;
 use slog::Logger;
 
 use config;
-use {Error, ErrorKind};
+use Error;
 
 pub(crate) struct FullSync {
     future: Box<Future<Item = (), Error = Error> + Send + 'static>,
@@ -30,10 +27,7 @@ impl FullSync {
             .unwrap_or(ObjectVersion(0));
         let create_object_table = make_create_object_table(logger.clone(), machine);
 
-        let chunk_size = 100; // TODO to be determined
         let step = 100; // TODO to be determined
-        #[allow(deprecated)]
-        let (deleted_objects_tx, deleted_objects_rx) = sync_channel(chunk_size);
 
         let logger = logger.clone();
         let logger2 = logger.clone();
@@ -41,20 +35,14 @@ impl FullSync {
 
         let combined_future = create_object_table
             .and_then(move |object_table| {
-                let list_deleted_content = make_list_content(
+                make_list_and_delete_content(
                     &logger,
                     &device,
                     node_id,
                     object_version_limit,
                     step,
-                    deleted_objects_tx,
                     object_table,
-                );
-                let delete_objects_in_stream =
-                    make_delete_objects_in_stream(&logger, &device, deleted_objects_rx, node_id);
-                list_deleted_content
-                    .join(delete_objects_in_stream)
-                    .map(move |((), ())| ())
+                )
             })
             .map(move |()| debug!(logger2, "FullSync objects done"));
 
@@ -86,89 +74,70 @@ impl Future for FullSync {
     }
 }
 
-// [0..object_version_limit) will be checked.
-pub fn make_list_content<
-    S: Sink<SinkItem = ObjectVersion, SinkError = std::sync::mpsc::SendError<ObjectVersion>>
-        + Send
-        + 'static,
->(
+pub fn make_list_and_delete_content(
     logger: &Logger,
     device: &DeviceHandle,
     node_id: NodeId,
     object_version_limit: ObjectVersion,
     step: u64,
-    sink: S,
     object_table: Vec<u64>,
 ) -> impl Future<Item = (), Error = Error> + Send {
+    assert!(step > 0);
     let logger = logger.clone();
     let device = device.clone();
-    debug!(logger, "Starts listing content");
-    let sink = sink.sink_map_err(|_| Error::from(ErrorKind::Other));
+    info!(
+        logger,
+        "Starts listing and deleting content, object_version_limit = {:?}, step = {}",
+        object_version_limit,
+        step
+    );
     loop_fn(
-        (ObjectVersion(0), sink, object_table),
-        move |(current_version, sink, object_table)| {
+        (ObjectVersion(0), object_table),
+        move |(current_version, object_table)| {
             let start_lump_id = config::make_lump_id(&node_id, current_version);
             let end_object_version = std::cmp::min(
                 ObjectVersion(current_version.0 + step),
                 object_version_limit,
             );
             let end_lump_id = config::make_lump_id(&node_id, end_object_version);
+            let device_cloned = device.clone();
             device
                 .request()
                 .deadline(Deadline::Infinity)
                 .list_range(start_lump_id..end_lump_id)
-                .map_err(From::from)
                 .and_then(move |lump_ids| {
                     let deleted_objects =
                         FullSync::compute_deleted_versions(lump_ids, &object_table);
-                    loop_fn((sink, deleted_objects), move |(sink, mut objects)| {
+                    loop_fn(deleted_objects, move |mut objects| {
                         if let Some(object) = objects.pop() {
+                            let lump_id = config::make_lump_id(&node_id, object);
                             Box::new(
-                                sink.send(object)
-                                    .map(|sink| Loop::Continue((sink, objects))),
+                                device_cloned
+                                    .request()
+                                    .deadline(Deadline::Infinity)
+                                    .delete(lump_id)
+                                    .map(move |_result| Loop::Continue(objects))
+                                    .map_err(From::from),
                             )
-                                as Box<dyn Future<Item = _, Error = _> + Send>
+                                as Box<dyn Future<Item = Loop<_, _>, Error = _> + Send>
                         } else {
-                            Box::new(ok(Loop::Break(sink)))
-                                as Box<dyn Future<Item = _, Error = _> + Send>
+                            Box::new(ok(Loop::Break(())))
+                                as Box<dyn Future<Item = Loop<_, _>, Error = _> + Send>
                         }
                     })
-                    .map(|sink| (sink, object_table))
+                    .map(move |()| object_table)
                 })
-                .and_then(move |(sink, object_table)| {
+                .and_then(move |object_table| {
                     let next = ObjectVersion(current_version.0 + step);
                     if next >= object_version_limit {
                         Ok(Loop::Break(()))
                     } else {
-                        Ok(Loop::Continue((next, sink, object_table)))
+                        Ok(Loop::Continue((next, object_table)))
                     }
                 })
                 .map_err(From::from)
         },
     )
-}
-
-/// Creates a future that asynchronously deletes objects enumerated by stream.
-fn make_delete_objects_in_stream<S: Stream<Item = ObjectVersion, Error = ()> + Send + 'static>(
-    logger: &Logger,
-    device: &DeviceHandle,
-    stream: S,
-    node_id: NodeId,
-) -> impl Future<Item = (), Error = Error> + Send {
-    let logger = logger.clone();
-    let device = device.clone();
-    stream
-        .map_err(|()| Error::from(ErrorKind::Other))
-        .for_each(move |object_version| {
-            let lump_id = config::make_lump_id(&node_id, object_version);
-            info!(logger, "Repair: deleting {:?}", lump_id);
-            device
-                .request()
-                .deadline(Deadline::Infinity)
-                .delete(lump_id)
-                .map(|_result| ())
-                .map_err(From::from)
-        })
 }
 
 pub fn make_create_object_table(
@@ -180,6 +149,7 @@ pub fn make_create_object_table(
         .async_call(move || get_object_table(&logger, &machine))
         .map_err(From::from)
 }
+
 fn get_object_table(logger: &Logger, machine: &Machine) -> Vec<u64> {
     let result = machine.enumerate_object_versions();
     let mut objects_count = 0;
@@ -200,49 +170,66 @@ mod tests {
     use cannyls::deadline::Deadline;
     use cannyls::device::DeviceHandle;
     use cannyls::lump::LumpId;
-    use config::make_lump_id;
-    use frugalos_mds::machine::Machine;
-    use full_sync::{make_create_object_table, make_list_content, FullSync};
-    use futures::stream::Stream;
+    use full_sync::{make_create_object_table, make_list_and_delete_content, FullSync};
+    use futures::future::Future;
     use libfrugalos::entity::object::{Metadata, ObjectVersion};
     use libfrugalos::expect::Expect;
     use slog::{Discard, Logger};
+    use std::{thread, time};
     use test_util::tests::{setup_system, wait, System};
     use trackable::result::TestResult;
 
+    use config::make_lump_id;
+    use fibers::executor::Executor;
+    use frugalos_mds::machine::Machine;
+
     #[test]
-    fn list_content_works_correctly() -> TestResult {
+    fn list_and_delete_content_works_correctly() -> TestResult {
         let logger = Logger::root(Discard, o!());
         let mut system = System::new(2, 1)?;
         let (nodes, _client) = setup_system(&mut system, 3)?;
         let (node0, _device_id0, handle0): (_, _, DeviceHandle) = nodes[0].clone();
 
+        thread::spawn(move || loop {
+            system.executor.run_once().unwrap();
+            thread::sleep(time::Duration::from_micros(100));
+        });
+
         // Puts 10 objects to device0
         for i in 0..10 {
             let lump_id = make_lump_id(&node0, ObjectVersion(i));
             let lump_data = handle0.allocate_lump_data_with_bytes(&[0; 10])?;
-            handle0
-                .request()
-                .deadline(Deadline::Infinity)
-                .put(lump_id, lump_data);
+            wait(
+                handle0
+                    .request()
+                    .deadline(Deadline::Immediate)
+                    .put(lump_id, lump_data)
+                    .map_err(From::from),
+            )?;
         }
 
         let object_table = vec![0];
 
-        #[allow(deprecated)]
-        let (sink, source) = fibers::sync::mpsc::sync_channel(100);
-
-        wait(make_list_content(
+        wait(make_list_and_delete_content(
             &logger,
             &handle0,
             node0,
             ObjectVersion(10),
             1,
-            sink,
             object_table,
         ))?;
-        let array: Vec<_> = source.wait().collect();
-        assert_eq!(array.len(), 10);
+
+        // Assert objects are deleted and therefore not found
+        for i in 0..10 {
+            let lump_id = make_lump_id(&node0, ObjectVersion(i));
+            assert!(!wait(
+                handle0
+                    .request()
+                    .deadline(Deadline::Immediate)
+                    .delete(lump_id)
+                    .map_err(From::from)
+            )?);
+        }
         Ok(())
     }
 
