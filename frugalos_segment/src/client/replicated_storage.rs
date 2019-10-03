590a898bcd1698cb3e6868f8e3e90e07a57c5488
brly <receive@brly.net>
2019-10-03 15:57:07 +0900

    Add TODO comments

--- frugalos_segment/src/client/replicated_storage.rs
+++ frugalos_segment/src/client/replicated_storage.rs
@@ -69,6 +69,7 @@ impl ReplicatedClient {
         };
         Box::new(future)
     }
+    /// TODO 実装
     pub fn head(self, _version: ObjectVersion, _deadline: Deadline) -> BoxFuture<()> {
         Box::new(futures::future::ok(()))
     }
