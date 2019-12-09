6dcba22876c22615edf48a214c5749bed53c52da
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-12-09 23:13:24 +0900

    SegmentGcManager becomes Future

--- frugalos_segment/src/segment_gc_manager.rs
+++ frugalos_segment/src/segment_gc_manager.rs
@@ -1,76 +1,105 @@
-/// 実行開始と実行中かどうかの確認ができる型
+use futures::{Async, Future, Poll};
+use slog::Logger;
+
+type UnitFuture = Box<dyn Future<Item = (), Error = ()>>;
+
+/// 実行開始と停止ができる型
 pub(crate) trait Toggle {
-    fn is_running(&self) -> bool;
-    fn start(&self);
-    fn stop(&self);
+    fn start(&self) -> UnitFuture;
+    fn stop(&self) -> UnitFuture;
 }
 
 /// segment_gc の実行状況の管理を行う。
 /// これは SegmentService に管理され、SegmentService の指示によって segment_gc を開始したりする。
 /// Task は SegmentHandle のラッパを入れることを想定された型変数である。テストのしやすさのために多相型にしている。
 pub(crate) struct SegmentGcManager<Task> {
+    logger: Logger,
     tasks: Vec<Task>,
-    running: Vec<usize>, // 実行中のタスクの番号の列
-    waiting: Vec<usize>, // 実行待ちのタスクの番号の列
-    limit: usize,        // 実行可能なタスク数の上限
+    running: Vec<(usize, UnitFuture)>,  // 実行中のタスクの番号の列
+    waiting: Vec<usize>,                // 実行待ちのタスクの番号の列
+    stopping: Vec<(usize, UnitFuture)>, // 停止中のタスクの番号の列
+    limit: usize,                       // 実行可能なタスク数の上限
 }
 
 impl<Task: Toggle> SegmentGcManager<Task> {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(logger: Logger) -> Self {
         Self {
+            logger,
             tasks: Vec::new(),
             running: Vec::new(),
             waiting: Vec::new(),
+            stopping: Vec::new(),
             limit: 0,
         }
     }
     /// 初期化する。今まで覚えていたデータは失われる。
     /// また今まで実行していたタスクも中断される。
+    /// タスクの実行中 (new してから set_limit をして、poll を始めた後) に呼んではいけない。
     pub(crate) fn init(&mut self, tasks: impl IntoIterator<Item = Task>) {
-        // Abort all tasks that were running
-        for &index in &self.running {
-            self.tasks[index].stop();
-        }
+        // タスクがどれも実行されていないという想定なので、単純に全て捨てる。
         self.tasks = tasks.into_iter().collect();
         self.running = Vec::new();
         self.waiting = (0..self.tasks.len()).collect();
+        self.stopping = Vec::new();
+        self.limit = 0;
     }
 
     /// 実行数のリミットを設定する。すでに実行中のタスクがある場合、リミットに収まらない分は中断される。
     pub(crate) fn set_limit(&mut self, limit: usize) {
         self.limit = limit;
         while self.running.len() > limit {
-            let index = self.running.pop().unwrap();
-            self.tasks[index].stop();
-            self.waiting.push(index);
+            let (index, _) = self.running.pop().unwrap();
+            let fut = self.tasks[index].stop();
+            self.stopping.push((index, fut));
         }
     }
+}
 
-    /// 全タスクが終わったかどうかチェックする
-    pub(crate) fn is_done(&mut self) -> bool {
-        self.update_info();
-        self.running.is_empty() && self.waiting.is_empty()
-    }
+impl<Task: Toggle> Future for SegmentGcManager<Task> {
+    type Item = ();
+    type Error = ();
 
-    /// この manager が持つ情報を最新にする。その結果 segment_gc の開始が必要そうならそうする。
-    fn update_info(&mut self) {
-        let new_running = self
-            .running
-            .iter()
-            .filter(|&&index| self.tasks[index].is_running())
-            .cloned()
-            .collect();
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let mut new_running = Vec::new();
+        // running -> discarded
+        for (index, mut fut) in std::mem::replace(&mut self.running, Vec::new()) {
+            match fut.poll() {
+                Ok(Async::NotReady) => new_running.push((index, fut)),
+                Ok(Async::Ready(())) => (),
+                Err(_) => {
+                    // ログだけ吐いて握り潰す
+                    warn!(self.logger, "Error executing running: index = {}", index);
+                }
+            }
+        }
         self.running = new_running;
-        // Fill in self.running with waiting tasks
+        // stopping -> waiting
+        let mut new_stopping = Vec::new();
+        for (index, mut fut) in std::mem::replace(&mut self.stopping, Vec::new()) {
+            match fut.poll() {
+                Ok(Async::NotReady) => new_stopping.push((index, fut)),
+                Ok(Async::Ready(())) => self.waiting.push(index),
+                Err(_) => {
+                    // ログだけ吐いて握り潰す
+                    warn!(self.logger, "Error executing running: index = {}", index);
+                }
+            }
+        }
+        self.stopping = new_stopping;
+        // fill
         while self.running.len() < self.limit {
             if let Some(index) = self.waiting.pop() {
-                self.tasks[index].start();
-                self.running.push(index);
+                // start index
+                self.running.push((index, self.tasks[index].start()));
             } else {
-                // No waiting tasks
                 break;
             }
         }
+        // 全ての仕事が終わったかどうか確認する。
+        if self.running.is_empty() && self.waiting.is_empty() && self.stopping.is_empty() {
+            return Ok(Async::Ready(()));
+        }
+        Ok(Async::NotReady)
     }
 }
 
@@ -83,13 +112,13 @@ mod tests {
     struct IncrementToggle(Arc<AtomicI32>);
 
     impl Toggle for IncrementToggle {
-        fn is_running(&self) -> bool {
-            false
-        }
-        fn start(&self) {
+        fn start(&self) -> UnitFuture {
             self.0.fetch_add(1, Ordering::SeqCst);
+            Box::new(futures::future::ok(()))
         }
-        fn stop(&self) {}
+        fn stop(&self) -> UnitFuture {
+            Box::new(futures::future::ok(()))
+        }
     }
 
     #[test]
@@ -100,10 +129,30 @@ mod tests {
         for _ in 0..count {
             tasks.push(IncrementToggle(Arc::clone(&counter)));
         }
-        let mut manager = SegmentGcManager::new();
+        let logger = Logger::root(slog::Discard, o!());
+        let mut manager = SegmentGcManager::new(logger);
         manager.init(tasks);
         manager.set_limit(1);
-        while !manager.is_done() {}
+        while let Ok(Async::NotReady) = manager.poll() {}
         assert_eq!(counter.load(Ordering::SeqCst), count);
+    }
+
+    #[test]
+    fn no_tasks_will_be_done_if_limit_is_zero() {
+        let count = 13;
+        let counter = Arc::new(AtomicI32::new(0));
+        let mut tasks = Vec::new();
+        for _ in 0..count {
+            tasks.push(IncrementToggle(Arc::clone(&counter)));
+        }
+        let logger = Logger::root(slog::Discard, o!());
+        let mut manager = SegmentGcManager::new(logger);
+        manager.init(tasks);
+        manager.set_limit(0);
+        // 十分な回数 poll しても NotReady のまま
+        for _ in 0..1000 {
+            assert_eq!(Ok(Async::NotReady), manager.poll())
+        }
+        assert_eq!(counter.load(Ordering::SeqCst), 0);
     }
 }
